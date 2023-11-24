a885c7f3b4b14fb1acf9eb8a8303c932098f40ef
AnushkaWijegoonawardana97 <anushkaduwolka123@gmail.com>
2023-11-24 21:28:09 +0530

    apply comments to the QuotesContent java class

--- src/base/QuotesContent.java
+++ src/base/QuotesContent.java
@@ -1,6 +1,11 @@
 package base;
 
+/**
+ * Represents a collection of quotes and their respective authors. The quotes
+ * are stored as a transient array of strings.
+ */
 public class QuotesContent {
+	// Array to store quotes and authors alternately
 	static transient String[] stuff = { "Progress comes from the intelligent use of experience.", "Elbert Hubbard", "",
 			"No amount of experimentation can ever prove me right; a single experiment can prove me wrong.",
 			"Albert Einstein", "", "To be trusted is a greater complement than to be loved.", "George MacDonald", "",
